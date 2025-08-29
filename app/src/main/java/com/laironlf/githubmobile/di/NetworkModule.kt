52ff7082700360c70bff97f6c0f96fc89498d595
laironlf <laironlf@gmail.com>
2025-08-30 06:06:34 +0700

    split DataModule into modules with related areas of responsibility.

---
+++ app/src/main/java/com/laironlf/githubmobile/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.laironlf.githubmobile.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.laironlf.githubmobile.data.api.github.GitHubApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubApiService(): GitHubApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(GitHubApiService::class.java)
+    }
+}
--- app/src/main/java/com/laironlf/githubmobile/di/RepositoryModule.kt
+++ app/src/main/java/com/laironlf/githubmobile/di/RepositoryModule.kt
@@ -1,31 +1,33 @@
 package com.laironlf.githubmobile.di
 
-import android.content.Context
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.laironlf.githubmobile.data.api.github.GitHubApiService
 import com.laironlf.githubmobile.data.api.github.GitHubRepositoryImpl
-import com.laironlf.githubmobile.data.storage.KeyValueStorageImpl
 import com.laironlf.githubmobile.domain.repository.AppRepository
 import com.laironlf.githubmobile.domain.repository.GitHubRepository
 import com.laironlf.githubmobile.domain.repository.KeyValueStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataModule {
+class RepositoryModule {
     @Provides
     @Singleton
-    fun provideKeyValueStorage(@ApplicationContext context: Context): KeyValueStorage {
-        return KeyValueStorageImpl(context = context)
+    fun provideAppRepository(
+        keyValueStorage: KeyValueStorage,
+        gitHubRepository: GitHubRepository
+    ): AppRepository {
+        return AppRepository(keyValueStorage = keyValueStorage, gitHubRepository = gitHubRepository)
     }
 
-
+    @Provides
+    @Singleton
+    fun provideGitHubRepository(
+        gitHubApiService: GitHubApiService
+    ): GitHubRepository {
+        return GitHubRepositoryImpl(gitHubApiService)
+    }
 }
