948ed25f5530dd54f9bcab37bed25f489d5fe8ba
laironlf <laironlf@gmail.com>
2025-08-30 05:49:31 +0700

    deleted the unnecessary GitHubRetrofitInstance.kt and moved everything into DataModule

--- app/src/main/java/com/laironlf/githubmobile/di/DataModule.kt
+++ app/src/main/java/com/laironlf/githubmobile/di/DataModule.kt
@@ -1,23 +1,30 @@
 package com.laironlf.githubmobile.di
 
 import android.content.Context
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.laironlf.githubmobile.data.api.github.GitHubApiService
 import com.laironlf.githubmobile.data.api.github.GitHubRepositoryImpl
-import com.laironlf.githubmobile.data.api.github.GitHubRetrofitInstance
-import com.laironlf.githubmobile.domain.repository.KeyValueStorage
 import com.laironlf.githubmobile.data.storage.KeyValueStorageImpl
 import com.laironlf.githubmobile.domain.repository.AppRepository
 import com.laironlf.githubmobile.domain.repository.GitHubRepository
+import com.laironlf.githubmobile.domain.repository.KeyValueStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
 
     @Provides
     @Singleton
@@ -28,7 +35,11 @@ class DataModule {
     @Provides
     @Singleton
     fun provideGitHubApiService(): GitHubApiService {
-        return GitHubRetrofitInstance().api
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(GitHubApiService::class.java)
     }
 
     @Provides
